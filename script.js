ad6370ac7d91a5e05c9c55c6b22b8029e359cb36
Javier <lessi111@icloud.com>
2024-09-03 13:49:53 -0400

    Add random color when grid item is hovered, cleaned row and column generation

--- script.js
+++ script.js
@@ -1,43 +1,51 @@
 let button = document.querySelector("#btnSquares");
 let gridSize = 16;
 
+function generateRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + "," + g + "," + b + ")";
+}
+
+function createNewGrid(gridSize) {
+  if (document.querySelector("#container")) {
+    document.querySelector("#container").remove();
+  }
+
+  let container = document.createElement("div");
+  container.setAttribute("id", "container");
+  container.style.display = "flex";
+  container.style.flexDirection = "column";
+
+  for (let i = 0; i < gridSize; i++) {
+    let row = document.createElement("div");
+    row.style.display = "flex";
+
+    for (let j = 0; j < gridSize; j++) {
+      let item = document.createElement("div");
+      item.style.width = 800 / gridSize + "px";
+      item.style.height = 800 / gridSize + "px";
+      item.style.border = "1px solid black";
+      item.addEventListener("mouseover", () => {
+        item.style.backgroundColor = generateRandomColor();
+      });
+
+      row.appendChild(item);
+    }
+    container.appendChild(row);
+  }
+
+  document.body.appendChild(container);
+}
+
 button.addEventListener("click", () => {
   gridSize = prompt("What size will the new grid be? (default: 16)", 16);
   if (gridSize <= 100) {
     createNewGrid(gridSize);
   } else {
-    alert("You can't make it more than 100!")
+    alert("You can't make it more than 100!");
   }
 });
-
-function createNewGrid(gridSize) {
-  if (document.querySelector(".container")) {
-    document.querySelector(".container").remove();
-  }
-
-  let container = document.createElement("div");
-  container.classList.add("container");
-
-  let row = document.createElement("div");
-  row.classList.add("row");
-
-  for (let i = 0; i < gridSize; i++) {
-    let item = document.createElement("div");
-    /*   item.addEventListener("mouseenter", () => {
-    item.style.backgroundColor = "red";
-  }) */
-    item.classList.add("item");
-    item.innerText = "";
-    row.appendChild(item);
-  }
-
-  for (let i = 0; i < gridSize; i++) {
-    // You can't append the same node multiple times
-    // hence, we clone it and then append it to container
-    rowClone = row.cloneNode(true);
-    container.appendChild(rowClone);
-  }
-  document.body.appendChild(container);
-}
 
 createNewGrid(gridSize);
